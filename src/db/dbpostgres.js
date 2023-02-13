8145046816e75feb0041741e2b0b3a2e87f089a2
André Paixão <69304541+andrepaixao81@users.noreply.github.com>
2023-02-13 01:59:23 -0300

    Update dbpostgres.js

--- src/db/dbpostgres.js
+++ src/db/dbpostgres.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const Pool = require('pg')
+//Conectando no banco por URL
 const pool = new Pool.Client(`${process.env.DB_URL}`)
 
 module.exports = pool
 
-//Conexão por paramertros
+//Conexão por parametros
 /*const pool = new Pool.Client({
   user: process.env.USER,
   host: process.env.HOST,
